4da660ea9f5feee6fb9e6542888b080c7f191d01
Shlok <shlokjagtap123@gmail.com>
2023-04-28 22:24:35 +0530

    index.css changes made

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-scroll';
 const Nav = () => {
   return <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
     <div className="container mx-auto">
-      <div className='w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between text-2xl text-white/50'>
+      <div className='w-full bg-black/20 h-[80px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between text-2xl text-white/50'>
         <Link to='home'  activeClass='active' smooth={true} spy={true} offset={-200}className='cursor-pointer w-[60px] flex items-center justify-center'>
         <BiHomeAlt/>
         </Link>
