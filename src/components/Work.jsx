a9aa585f7c9572ace9b61d2bebfc76222efd0119
Shlok <shlokjagtap123@gmail.com>
2023-04-28 19:39:49 +0530

    correction in Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { fadeIn } from '../variants'
+import { fadeIn } from '../variants';
 import img1 from '../assets/portfolio-img1.png';
 import img2 from '../assets/portfolio-img2.png';
 import img3 from '../assets/portfolio-img3.png';
 
-const Work = () => {
+export const Work = () => {
   return (
-    <section className='section' id='work'>
+    <section className='section'id='work'>
       <div className="container mx-auto">
         <div className='flex flex-col lg:flex-row gap-x-10'>
-          <div className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
+          <motion.div 
+          variants={fadeIn('right',0.3)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once:false,amount:0.3}}
+          className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
             {/* text */}
             <div>
             <h2 className='h2 leading-tight text-accent'>My Latest <br />
@@ -31,48 +36,51 @@ const Work = () => {
               <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
                 <span className='text-gradient'>UI/UX</span>
                  </div>
-              {/* title */}
+                  {/* title */}
               <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'> 
                 <span className='text-3xl text-white'>Project Title</span>
               </div>
             </div>
-          </div>
-          <div className='flex-1 flex flex-col gap-y-10'>
+          </motion.div>
+          <motion.div
+          variants={fadeIn('left',0.2)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once:false,amount:0.3}}
+           className='flex-1 flex flex-col gap-y-8 pt-10'>
             {/* image */}
             <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
               {/* overlay */}
               <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
               {/* img */}
-              <img className='group-hover:scale-125 transition-all duration-500' src={img1} alt="" />
+              <img className='group-hover:scale-125 transition-all duration-500' src={img2} alt="" />
               {/* pretitle */}
               <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
                 <span className='text-gradient'>UI/UX</span>
                  </div>
-              {/* title */}
+                  {/* title */}
               <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'> 
                 <span className='text-3xl text-white'>Project Title</span>
               </div>
             </div>
-             <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
               {/* overlay */}
               <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
               {/* img */}
-              <img className='group-hover:scale-125 transition-all duration-500' src={img1} alt="" />
+              <img className='group-hover:scale-125 transition-all duration-500' src={img3} alt="" />
               {/* pretitle */}
               <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
                 <span className='text-gradient'>UI/UX</span>
                  </div>
-              {/* title */}
+                  {/* title */}
               <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'> 
                 <span className='text-3xl text-white'>Project Title</span>
               </div>
             </div>
-
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
   );
 };
-
 export default Work;
