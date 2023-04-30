b04c248f315f9470222027492521680f8bf352f0
Shlok <shlokjagtap123@gmail.com>
2023-04-30 19:47:59 +0530

    fixed?

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -27,12 +27,12 @@ const About = () => {
          viewport={{once:false,amount:0.3}}
           className='flex-1'>
            <h2 className='h2 text-accent'>About me.</h2>
-           <h3 className='h3 mb-4'>I'm a Freelancer Front-end Developer and Block-chain Enthusiast .</h3>
-           <p className=' z-10'>
+           <h3 className='h3 mb-2'>I'm a Freelancer Front-end Developer and Block-chain Enthusiast .</h3>
+           <p className='mt-2'>
             My name is Shlok Jagtap, and I'm a Frontend developer based in Pune. I specialize in building modern web applications using React.
            </p>
            {/* stats */}
-           <div className='flex pt-10  gap-x-6 lg:gap-x-10 mt-10 mb-10'>
+           <div className='flex pt-8 gap-x-6 lg:gap-x-10 mt-10 mb-10'>
              <div>
                <div className='text-[40px] font-tertiary text-gradient mb-2'>
                  {inView? <CountUp start={0} end={2} duration={3}/>: null}
