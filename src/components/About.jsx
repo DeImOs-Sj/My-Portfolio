3210fd973e6a10c0c246f8b2893682a845ec472f
Shlok <shlokjagtap123@gmail.com>
2023-04-30 19:35:58 +0530

    fixed?

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,7 +32,7 @@ const About = () => {
             My name is Shlok Jagtap, and I'm a Frontend developer based in Pune. I specialize in building modern web applications using React.
            </p>
            {/* stats */}
-           <div className='flex gap-x-6 lg:gap-x-10 mt-10 mb-10'>
+           <div className='flex align-center gap-x-6 lg:gap-x-10 mt-10 mb-10'>
              <div>
                <div className='text-[40px] font-tertiary text-gradient mb-2'>
                  {inView? <CountUp start={0} end={2} duration={3}/>: null}
